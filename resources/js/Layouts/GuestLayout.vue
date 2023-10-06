49c24c3fae423356fa65d8f49ef182818fcdfa99
aron.biro <aron.biro@webgurus.io>
2023-10-06 11:49:03 +0300

    Initial commit

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div class="flex">
+        <div class="bg-blue-500 w-1/3"></div>
+        <div
+            class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 w-2/3"
+        >
+            <div
+                class="sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg w-full"
+            >
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
