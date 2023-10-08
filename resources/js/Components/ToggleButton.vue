a6e197e67a95a10254ac288070e6c3ff199f2dbd
aron.biro <aron.biro@webgurus.io>
2023-10-09 00:12:01 +0300

    DLP-1 Created custom Sig Up and Login pages

---
+++ resources/js/Components/ToggleButton.vue
@@ -0,0 +1,8 @@
+<template>
+    <label class="relative inline-flex items-center cursor-pointer">
+        <input type="checkbox" value="" class="sr-only peer" />
+        <div
+            class="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-2 rounded-full peer peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary"
+        ></div>
+    </label>
+</template>
